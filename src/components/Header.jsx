4f5d7161af328e6851d75daa6112942ba3d209ac
Francois Lacoursiere <lacouf@gmail.com>
2025-03-31 15:33:52 -0400

    Destructuration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 
-const Header = (props) => {
+const Header = ({title}) => {
     return (
         <header>
-            <h1>{props.title}</h1>
+            <h1>{title}</h1>
         </header>
     )
 }
