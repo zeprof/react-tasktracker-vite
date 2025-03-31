070004fa3c760df4b824c64d40bb1750fc3a0d08
Francois Lacoursiere <lacouf@gmail.com>
2025-03-31 17:15:40 -0400

    Ajout de bouton (1ere partie)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,10 @@
 const Header = ({title}) => {
     return (
         <header>
-            <h1 style={headingStyle}>{title}</h1>
+            <h1>{title}</h1>
+            <button className='btn'>Add</button>
         </header>
     )
 }
 
-const headingStyle = {
-    color: 'red', backgroundColor: 'black'
-}
 export default Header
