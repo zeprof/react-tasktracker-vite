f3e20ee93573e7df017d7fa43b0fc169eba2a94a
Francois Lacoursiere <lacouf@gmail.com>
2025-03-31 16:43:07 -0400

    Inline styling (2ieme facon)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,12 @@
 const Header = ({title}) => {
     return (
         <header>
-            <h1 style={{color: 'red', backgroundColor: 'black'}}>{title}</h1>
+            <h1 style={headingStyle}>{title}</h1>
         </header>
     )
 }
 
+const headingStyle = {
+    color: 'red', backgroundColor: 'black'
+}
 export default Header
