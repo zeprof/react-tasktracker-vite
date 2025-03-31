7bd05bd27f3ca8b4ebc961d295878ded7e158281
Francois Lacoursiere <lacouf@gmail.com>
2025-03-31 16:40:43 -0400

    Inline styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 const Header = ({title}) => {
     return (
         <header>
-            <h1>{title}</h1>
+            <h1 style={{color: 'red', backgroundColor: 'black'}}>{title}</h1>
         </header>
     )
 }
