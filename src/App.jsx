fad6e326f7e96ae19eb9cdfac544ed56daa010a2
Francois Lacoursiere <lacouf@gmail.com>
2025-03-31 15:10:39 -0400

    Variables dans jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    const name = 'Francois'
+    const x = false
   return (
-    <>
-      <div>
-      <h1>Hello React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      </div>
-    </>
+      <>
+          <h1>Hello from React</h1>
+          <h2>Allo {name}</h2>
+          <h2>Calcul {1 +1}</h2>
+          <h2>Conditionnel {x ? 'vrai' : 'faux'}</h2>
+      </>
   )
 
 }
