a66be10693828febbaa49005e49da7b5413e9af1
Francois Lacoursiere <lacouf@gmail.com>
2025-04-02 14:24:16 -0400

    Passe function du parent a composant Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,5 @@
 
-const Button = ({color, text}) => {
-    const onClick = () => {
-        console.log('button pressed')
-    }
-
+const Button = ({color, text, onClick}) => {
     return (
         <button
             onClick={onClick}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import Button from "./Button.jsx";
 
 const Header = ({title}) => {
+    const onClick = () => {
+        console.log('Click')
+    }
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Hello'/>
+            <Button color='green' text='Hello' onClick={onClick}/>
         </header>
     )
 }
-
 export default Header
