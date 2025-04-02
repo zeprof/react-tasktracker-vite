4e94bc560760c9d77fcf9bdf3fc5be609a9b914f
Francois Lacoursiere <lacouf@gmail.com>
2025-04-02 14:35:13 -0400

    utilise useState()

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,25 +1,28 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Preparer mon cours',
-        day: '19 Avril @ 14:25',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Commencer mon TP',
-        day: '20 Septembre @ 09:00',
-        reminder: false,
-    },
-    {
-        id: 3,
-        text: 'Faire des tests',
-        day: '23 Septembre @ 10:00',
-        reminder: false,
-    },
-]
+import {useState} from "react";
+
 
 const Tasks = () => {
+    const [tasks, setTasks] = useState([
+            {
+                id: 1,
+                text: 'Preparer mon cours',
+                day: '19 Avril @ 14:25',
+                reminder: true,
+            },
+            {
+                id: 2,
+                text: 'Commencer mon TP',
+                day: '20 Septembre @ 09:00',
+                reminder: false,
+            },
+            {
+                id: 3,
+                text: 'Faire des tests',
+                day: '23 Septembre @ 10:00',
+                reminder: false,
+            },
+    ]
+    )
     return (
         <>
             {tasks.map((task) => (
