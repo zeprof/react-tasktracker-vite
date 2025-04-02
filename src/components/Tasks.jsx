86dd657a9ca2fcfdd02c279ea708a5c2fac70a48
Francois Lacoursiere <lacouf@gmail.com>
2025-04-02 14:31:04 -0400

    Fix key issue with list

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -22,7 +22,9 @@ const tasks = [
 const Tasks = () => {
     return (
         <>
-            {tasks.map((task) => (<h3>{task.text}</h3>))}
+            {tasks.map((task) => (
+                <h3 key={task.id}>{task.text}</h3>
+            ))}
         </>
     )
 }
