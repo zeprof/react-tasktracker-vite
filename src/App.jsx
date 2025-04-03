78ee01c76ed5e356e14de1b3c472745a644bddcd
Francois Lacoursiere <lacouf@gmail.com>
2025-04-03 13:27:17 -0400

    change button depending if add form is shown or not

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,8 @@ function App() {
 
     return (
         <div className='container'>
-            <Header title="Task Tracker"/>
+            <Header title="Task Tracker" onAdd={() => setShowAddTask(!showAddTask)}
+                    showAdd={showAddTask}/>
             {showAddTask && <AddTask onAdd={addTask} />}
             {tasks.length > 0 ?
                 <Tasks tasks={tasks}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
 import Button from "./Button.jsx";
 
-const Header = ({title}) => {
-    const onClick = () => {
-        console.log('Click')
-    }
+const Header = ({title, onAdd, showAdd}) => {
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
+            <Button color={showAdd ? 'red' : 'green'}
+                    text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
         </header>
     )
 }
