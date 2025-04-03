837db209e0e535c3aaef828b88603215a3849518
Francois Lacoursiere <lacouf@gmail.com>
2025-04-03 13:23:16 -0400

    show task form depending on header button

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {useState} from "react";
 
 
 function App() {
+    const [showAddTask, setShowAddTask] = useState(false)
     const [tasks, setTasks] = useState([
             {
                 id: 1,
@@ -51,7 +52,7 @@ function App() {
     return (
         <div className='container'>
             <Header title="Task Tracker"/>
-            <AddTask onAdd={addTask} />
+            {showAddTask && <AddTask onAdd={addTask} />}
             {tasks.length > 0 ?
                 <Tasks tasks={tasks}
                        onDelete={deleteTask}
