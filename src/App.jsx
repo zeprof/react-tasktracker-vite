b3c663796e079b42f6b40d84af43d9d39ce5b398
Francois Lacoursiere <lacouf@gmail.com>
2025-04-09 16:03:57 -0400

    Ajoute Update, Add, Delete au BE - FIN Todo React + Spring Boot

--- src/App.jsx
+++ src/App.jsx
@@ -25,21 +25,50 @@ function App() {
         return data
     }
 
-    const addTask = (task) => {
-        const id = Math.floor(Math.random() * 10000) + 1
-        const newTask = {id, ...task}
-        setTasks([...tasks, newTask])
+    const fetchTask = async(id) => {
+        const res = await fetch(`http://localhost:8080/todos/${id}`)
+        const data = await res.json()
+        return data
     }
 
-    const deleteTask = (id) => {
+    const addTask = async (task) => {
+        const res = await fetch('http://localhost:8080/todos',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(task)
+            })
+        const data = await res.json()
+        setTasks([...tasks, data])
+    }
+
+    const deleteTask = async (id) => {
+        await fetch(`http://localhost:8080/todos/${id}`, {
+            method: 'DELETE'
+        })
         setTasks(tasks.filter((task) => task.id !== id))
     }
 
-    const toggleReminder = (id) => {
+    const toggleReminder = async (id) => {
+        const taskToToggle = await fetchTask(id)
+        const updTask = {...taskToToggle,
+            reminder: !taskToToggle.reminder}
+
+        await fetch(`http://localhost:8080/todos/${id}`,
+            {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(updTask)
+            })
+
         setTasks(
             tasks.map(
                 (task) => task.id === id ?
-                    {...task, reminder: !task.reminder} : task
+                    {...task, reminder: updTask.reminder} : task
             )
         )
     }
